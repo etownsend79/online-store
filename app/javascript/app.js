1367b2285f0ff90c053f2b10abc1f8e44f7f03ee
etownsend79 <earltownsend1979@gmail.com>
2020-07-07 16:52:52 -0500

    create category table and styles

--- app/javascript/app.js
+++ app/javascript/app.js
@@ -4,9 +4,17 @@ import "@fortawesome/fontawesome-free/js/all";
 document.addEventListener("turbolinks:load", function () {
   const hamburger = document.querySelector("#hamburger");
   const navLink = document.getElementById('nav-links');
+  const category = document.getElementById('category');
+  const genre = document.getElementById('genre');
 
   hamburger.addEventListener("click", () => {
     navLink.classList.toggle("show");
+  })
+
+  genre.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    category.classList.toggle("show-ul");
   })
 });
 
